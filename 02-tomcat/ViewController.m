43d2d0e49c3af5ffb8938b33cdb95dceb153d894
zhanglu1988em <zhanglu1988em163.com@zhanglu1988emdemacbook-pro.local>
2016-01-02 14:53:22 +0800

    tomcat 功能实现

--- 02-tomcat/ViewController.m
+++ 02-tomcat/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
@@ -22,6 +24,36 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)knockout:(UIButton *)sender {
+    [self perfAnimationWithName:@"knockout" :81];
+    
+}
+
+- (IBAction)eatBird:(id)sender {
+
+    [self perfAnimationWithName:@"eat" :40];
+    
+    
+}
+
+-(void) perfAnimationWithName :(NSString*)name :(int) imageCount
+{
+    
+    if ([self.imageView isAnimating]) {
+        return;
+    }
+    NSMutableArray *arryM = [NSMutableArray array];
+    for (int i = 0; i < imageCount;  ++i ) {
+        NSString *imageName = [NSString stringWithFormat:@"%@_%02d.jpg",name,i];
+        [arryM addObject:[UIImage imageNamed:imageName]];
+    }
+    [self.imageView setAnimationImages:arryM];
+    [self.imageView setAnimationDuration: imageCount * 0.075];
+    [self.imageView setAnimationRepeatCount:1];
+    [self.imageView startAnimating];
 }
 
 @end
